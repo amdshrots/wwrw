22c7c8f342e378b55009f2424a32b77f335bdb76
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-25 10:16:42 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
-# setup.sh
-# Install VNC server
-brew install --cask vnc-server  # For macOS
+#!/bin/bash
 
-# Set VNC password (replace 'yourpassword' with a secure value)
-echo "yourpassword" | vncpasswd -f > ~/.vnc/passwd
+# Install TigerVNC (includes vncserver and vncpasswd)
+brew install vnc  # [[8]]
+
+# Set VNC password
+echo "password" | vncpasswd -f > ~/.vnc/passwd  # [[8]]
 chmod 600 ~/.vnc/passwd
 
-# Start VNC server on default port 5900
-vncserver :0 -geometry 1920x1080 -depth 24
+# Start VNC server on display :1
+vncserver :1 -geometry 1920x1080 -depth 24 -localhost no  # [[8]]
+
+# Setup Playit.gg tunnel
+curl -sSL https://raw.githubusercontent.com/aBoredDev/playit-setup-script/main/install.sh | bash  # [[3]]
+playit --secret $PLAYIT_SECRET tcp://localhost:5901  # [[3]]
