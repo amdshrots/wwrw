33e08de88d1c5f32725c6ad75acd3aa9d99b3908
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-25 10:25:08 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,56 @@
-#!/bin/bash
+sudo mdutil -i off -a
 
-# Install TigerVNC (includes vncserver and vncpasswd)
-brew install vnc  # [[8]]
+sudo dscl . -create /Users/akhil
+sudo dscl . -create /Users/akhil UserShell /bin/bash
+sudo dscl . -create /Users/akhil RealName "Akhil"
+sudo dscl . -create /Users/akhil UniqueID 1001
+sudo dscl . -create /Users/akhil PrimaryGroupID 80
+sudo dscl . -create /Users/akhil NFSHomeDirectory /Users/vncuser
+sudo dscl . -passwd /Users/akhil $1
+sudo dscl . -passwd /Users/akhil $1
+sudo createhomedir -c -u akhil > /dev/null
 
-# Set VNC password
-echo "password" | vncpasswd -f > ~/.vnc/passwd  # [[8]]
-chmod 600 ~/.vnc/passwd
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-# Start VNC server on display :1
-vncserver :1 -geometry 1920x1080 -depth 24 -localhost no  # [[8]]
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-# Setup Playit.gg tunnel
-curl -sSL https://raw.githubusercontent.com/aBoredDev/playit-setup-script/main/install.sh | bash  # [[3]]
-playit --secret $PLAYIT_SECRET tcp://localhost:5901  # [[3]]
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+
+sudo nvram boot-args="serverperfmode=1 $(nvram boot-args 2>/dev/null | cut -f 2-)"
+
+defaults write com.apple.Accessibility DifferentiateWithoutColor -int 1
+defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
+defaults write com.apple.universalaccess reduceMotion -int 1
+defaults write com.apple.universalaccess reduceTransparency -int 1
+defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
+
+sudo /usr/bin/defaults write .GlobalPreferences MultipleSessionsEnabled -bool TRUE
+
+defaults write "Apple Global Domain" MultipleSessionsEnabled -bool true
+
+defaults write com.apple.loginwindow DisableScreenLock -bool true
+
+defaults write com.apple.loginwindow AllowList -string '*'
+
+
+brew install --cask google-chrome
+brew install --cask chrome-remote-desktop-host
+brew install --cask teamviewer
+brew install --cask anydesk
+
+defaults write com.apple.universalaccessAuthWarning "/Applications/AnyDesk.app" -bool true
+defaults write com.apple.universalaccessAuthWarning "/Applications/AnyDesk.app/Contents/MacOS/AnyDesk" -bool true
+defaults write com.apple.universalaccessAuthWarning "3::/Applications" -bool true
+defaults write com.apple.universalaccessAuthWarning "3::/Applications/AnyDesk.app" -bool true
+defaults write com.apple.universalaccessAuthWarning "com.philandro.anydesk" -bool true
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+
+git clone https://github.com/playit-cloud/playit-agent.git
+cd playit-agent
+cargo build --release
+
+sudo mv target/release/playit /usr/local/bin/
