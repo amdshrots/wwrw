20dd74fbe952278293824d2237868fc78292fd39
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-25 13:28:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,7 +58,6 @@ brew upgrade zrok
 export PATH="/usr/local/bin:$PATH"  # Add this line
 zrok config set apiEndpoint https://api-v1.zrok.io
 zrok enable $ZROK_TOKEN
-zrok list
 
 # Schedule shutdown after 6 hours
 echo "Scheduling shutdown in 6 hours..."
