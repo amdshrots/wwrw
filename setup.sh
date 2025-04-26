c19a2a9b04a6057685eb92bab74a80d09822dc0a
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 05:19:26 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ sudo defaults write /Library/Preferences/.GlobalPreferences MultipleSessionsEnab
 # Disable screen lock
 defaults write com.apple.loginwindow DisableScreenLock -bool YES
 
-brew install --cask zrok
+brew install zrok
 brew install --cask brave-browser
 brew install --cask chrome-remote-desktop-host
 
