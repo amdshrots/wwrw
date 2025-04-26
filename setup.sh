f44217f5a7811fe6138674e226de575724fcf33c
amdshrots <159141089+amdshrots@users.noreply.github.com>
2025-04-26 05:13:24 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,55 +1,49 @@
-#!/bin/bash
+# setup.sh VNC_USER_PASSWORD VNC_PASSWORD ZROK_TOKEN
 
 # Disable spotlight indexing
 sudo mdutil -i off -a
 
 # Create new account
-sudo dscl . -create /Users/vncuser
-sudo dscl . -create /Users/vncuser UserShell /bin/bash
-sudo dscl . -create /Users/vncuser RealName "VNC User"
-sudo dscl . -create /Users/vncuser UniqueID 1001
-sudo dscl . -create /Users/vncuser PrimaryGroupID 80
-sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-sudo dscl . -passwd /Users/vncuser $1
-sudo dscl . -passwd /Users/vncuser $1
-sudo createhomedir -c -u vncuser > /dev/null
+sudo dscl . -create /Users/akhil
+sudo dscl . -create /Users/akhil UserShell /bin/bash
+sudo dscl . -create /Users/akhil RealName "Akhil"
+sudo dscl . -create /Users/akhil UniqueID 1001
+sudo dscl . -create /Users/akhil PrimaryGroupID 80
+sudo dscl . -create /Users/akhil NFSHomeDirectory /Users/vncuser
+sudo dscl . -passwd /Users/akhil $1
+sudo createhomedir -c -u akhil > /dev/null
 
 # Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-# VNC password
-echo $1 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+# Set VNC password
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-# Start VNC/reset changes
+# Restart VNC service
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-# Enable Performance mode
+# Performance optimizations
 sudo nvram boot-args="serverperfmode=1 $(nvram boot-args 2>/dev/null | cut -f 2-)"
-
-# Reduce Motion and Transparency 
-defaults write com.apple.Accessibility DifferentiateWithoutColor -int 1
 defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
 defaults write com.apple.universalaccess reduceMotion -int 1
 defaults write com.apple.universalaccess reduceTransparency -int 1
-defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
 
-# Enable Multi-Session
-sudo /usr/bin/defaults write .GlobalPreferences MultipleSessionsEnabled -bool TRUE
+# Enable multi-session
+sudo defaults write /Library/Preferences/.GlobalPreferences MultipleSessionsEnabled -bool YES
 
-defaults write "Apple Global Domain" MultipleSessionsEnabled -bool true
+# Disable screen lock
+defaults write com.apple.loginwindow DisableScreenLock -bool YES
 
-# Disable Screen-Lock
-defaults write com.apple.loginwindow DisableScreenLock -bool true
+# Install tools
+cd ~/Downloads && mkdir -p /tmp/zrok && tar -xf ./zrok*darwin*.tar.gz -C /tmp/zrok
+mkdir -p ~/bin && install /tmp/zrok/zrok ~/bin/
+PATH=~/bin:$PATH
 
-defaults write com.apple.loginwindow AllowList -string '*'
+brew install --cask brave-browser
+brew install --cask chrome-remote-desktop-host
 
-brew install zrok
-
-set -e  
-echo "Starting zrok tunnel..."
-zrok enable --token "$ZROK_TOKEN" tcp 5900 || { echo "zrok failed"; exit 1; }
-
-echo "Scheduling shutdown in 6 hours..."
-sudo shutdown -h +360
+# Configure zrok
+zrok auth $3
+zrok tunnel tcp 5900 --name my-vnc-tunnel --region in &
